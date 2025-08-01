98d2afc0252c51cee2f200c6342f855445138cf0
AndresCastillo15 <andrescastillo@colegioguanenta.edu.co>
2025-08-01 06:45:24 -0500

    Funcionalidad de botones

--- interfaz_1.py
+++ interfaz_1.py
@@ -5,29 +5,47 @@ import sys
 pygame.init()
 
 # Colores
-negro = (0,0,0)
-blanco = (255,255,255)
-Celeste	= (135, 206, 250)
+negro = (0, 0, 0)
+blanco = (255, 255, 255)
+Celeste = (135, 206, 250)
 
-# imagenes
-
-boton_ajustes = pygame.image.load("img/AJUSTES.png")
-boton_ajustes = pygame.transform.scale(boton_ajustes, (200,50))
-boton_jugar = pygame.image.load("img/JUGAR.png")
-boton_jugar = pygame.transform.scale(boton_jugar, (200,50))
-boton_creditos = pygame.image.load("img/CREDITOS.png")
-boton_creditos = pygame.transform.scale(boton_creditos, (200,50))
-logo_juego = pygame.image.load("img/logo_juego.png")
-
-# tamaño de pantalla
+# Tamaño de pantalla
 info = pygame.display.Info()
 ancho = info.current_w
 alto = info.current_h
-
-# ventana maximizada con opción de redimensionar
 ventana = pygame.display.set_mode((ancho, alto), pygame.RESIZABLE)
 pygame.display.set_caption("CranICFES")
 
+# Cargar imágenes
+boton_ajustes_img = pygame.image.load("img/AJUSTES.png")
+boton_ajustes = pygame.transform.scale(boton_ajustes_img, (200, 50))
+boton_ajustes_hover = pygame.transform.scale(boton_ajustes_img, (220, 55))
+
+boton_jugar_img = pygame.image.load("img/JUGAR.png")
+boton_jugar = pygame.transform.scale(boton_jugar_img, (200, 50))
+boton_jugar_hover = pygame.transform.scale(boton_jugar_img, (220, 55))  
+
+boton_creditos_img = pygame.image.load("img/CREDITOS.png")
+boton_creditos = pygame.transform.scale(boton_creditos_img, (200, 50))
+boton_creditos_hover = pygame.transform.scale(boton_creditos_img, (220,55))
+
+logo_juego = pygame.image.load("img/logo_juego.png")
+
+# Rectángulos para los botones (posiciones base)
+pos_ajustes = (220, 580)
+rect_ajustes = boton_ajustes.get_rect(topleft=pos_ajustes)
+rect_ajustes_hover = boton_ajustes_hover.get_rect(center=rect_ajustes.center)
+
+pos_jugar = (550, 580)
+rect_jugar = boton_jugar.get_rect(topleft=pos_jugar)
+rect_jugar_hover = boton_jugar_hover.get_rect(center=rect_jugar.center)
+
+pos_creditos = (880, 580)
+rect_creditos = boton_creditos.get_rect(topleft=pos_creditos)
+rect_creditos_hover = boton_creditos_hover.get_rect(center=rect_creditos.center)
+
+# Estado de la pantalla
+pantalla_actual = "menu"
 
 # Bucle principal
 corriendo = True
@@ -39,20 +57,60 @@ while corriendo:
             if evento.key == pygame.K_ESCAPE:
                 corriendo = False
         elif evento.type == pygame.VIDEORESIZE:
-            # Actualizar tamaño si el usuario redimensiona
-            pantalla = pygame.display.set_mode((evento.w, evento.h), pygame.RESIZABLE)
-            
-    # Dibujar fondo 
-    ventana.fill((Celeste))
+            ventana = pygame.display.set_mode((evento.w, evento.h), pygame.RESIZABLE)
+        elif evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
+            mouse_pos = pygame.mouse.get_pos()
+            if pantalla_actual == "menu":
+                if rect_ajustes.collidepoint(mouse_pos):
+                    pantalla_actual = "ajustes"
+                elif rect_jugar.collidepoint(mouse_pos):
+                    pantalla_actual = "jugar"
+                elif rect_creditos.collidepoint(mouse_pos):
+                    pantalla_actual = "creditos"
 
-    ventana.blit(boton_ajustes, (220,580))
-    ventana.blit(boton_jugar, (550,580))
-    ventana.blit(boton_creditos, (880,580))
-    ventana.blit(logo_juego, (400,50))
+    # Dibujar según la pantalla actual
+    if pantalla_actual == "menu":
+        ventana.fill(Celeste)
+        mouse_pos = pygame.mouse.get_pos()
 
-    # Obtener tamaño actual de ventana
-    ancho_actual, alto_actual = ventana.get_size()
+        # Botón AJUSTES con efecto hover
+        if rect_ajustes.collidepoint(mouse_pos):
+            ventana.blit(boton_ajustes_hover, rect_ajustes_hover)
+        else:
+            ventana.blit(boton_ajustes, rect_ajustes)
 
+        # Botón JUGAR con efecto hover
+        if rect_jugar.collidepoint(mouse_pos):
+            ventana.blit(boton_jugar_hover, rect_jugar_hover)
+        else:
+            ventana.blit(boton_jugar, rect_jugar)
+
+        # Botón Créditos 
+        if rect_creditos.collidepoint(mouse_pos):
+            ventana.blit(boton_creditos_hover, rect_creditos_hover)
+        else:
+            ventana.blit(boton_creditos, rect_creditos)
+
+        # Logo
+        ventana.blit(logo_juego, (400, 50))
+
+    elif pantalla_actual == "ajustes":
+        ventana.fill((200, 200, 255))  # Fondo diferente
+        font = pygame.font.SysFont(None, 60)
+        texto = font.render("Pantalla de Ajustes", True, negro)
+        ventana.blit(texto, (ancho // 2 - texto.get_width() // 2, alto // 2 - 30))
+
+    elif pantalla_actual == "jugar":
+        ventana.fill((255, 255, 200))  
+        font = pygame.font.SysFont(None, 60)
+        texto = font.render("juego:)", True, negro)
+        ventana.blit(texto, (ancho // 2 - texto.get_width() // 2, alto // 2 - 30))
+    
+    elif pantalla_actual == "creditos":
+        ventana.fill((255, 255, 200))  
+        font = pygame.font.SysFont(None, 60)
+        texto = font.render("creditos", True, negro)
+        ventana.blit(texto, (ancho // 2 - texto.get_width() // 2, alto // 2 - 30))
 
     pygame.display.flip()
 
